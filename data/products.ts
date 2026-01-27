c673fd22ce8cb8bcad4e70dda07d215d12b376e3
devmani <devmani1456@gmail.com>
2026-01-27 23:16:34 +0545

    build fixed

--- data/products.ts
+++ data/products.ts
@@ -170,7 +170,6 @@ export const products: Product[] = [
     brand: "Microsoft",
     category: "Gaming",
     image: "/images/products/Gaming.jpg",
-    label: "NEW ARRIVAL",
   },
   {
     id: "15",
