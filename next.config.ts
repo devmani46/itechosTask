a7902ab93221921e302797c84610927b260cb8a5
devmani <devmani1456@gmail.com>
2026-01-28 12:03:51 +0545

    fixing build 4

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
